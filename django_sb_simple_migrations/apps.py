d1cea45cfb60d2b9b8f7bd869740461c05d7883c
zodiacfireworks <martin.vuelta@gmail.com>
2021-06-18 14:14:19 -0500

    Apps config options, add version file

--- django_sb_simple_migrations/apps.py
+++ django_sb_simple_migrations/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class DjangoSbSimpleMigrationsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "django_sb_simple_migrations"
+    verbose_name = "Django simple migrations"
