7ba0e10d57dbea715997b3f07014268806aeb4bd
zodiacfireworks <martin.vuelta@gmail.com>
2023-08-05 16:27:51 -0500

    Update: add base classes for tests

---
+++ tests/test_migrate.py
@@ -0,0 +1,8 @@
+from django.test import TestCase
+from django_sb_simple_migrations.management.commands.migrate import (
+    Command as MigrateCommand,
+)
+
+
+class SimpleMigrationsTestCase(TestCase):
+    command = MigrateCommand
